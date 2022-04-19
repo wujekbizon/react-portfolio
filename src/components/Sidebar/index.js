ff00dc576a989bbda25a145f012b41a26ddb2fd5
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-19 09:34:11 +0200

    sidebar layout complete add css style add new logo images

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,58 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import LogoG from '../../assets/images/logoGv2.png'
+import LogoSub from '../../assets/images/logo_sub-v3.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
-    <Link></Link>
+    <Link className="logo" to="/">
+      <img src={LogoG} alt="logo" />
+      <img className="sub-logo" src={LogoSub} alt="wolfinger" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/grzegorz-wolfinger-b88856229/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/wujekbizon"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
