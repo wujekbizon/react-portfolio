6a1161a6beccb0e1fc4edda88a557d998e89add2
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-22 22:40:56 +0200

    add hamburger navbar button on mobile size

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,55 +5,88 @@ import LogoSub from '../../assets/images/logo_sub-v3.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
+import { MdClose } from 'react-icons/md'
+import { FiMenu } from 'react-icons/fi'
 
-const Sidebar = () => (
-  <div id="nav_bar" className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoG} alt="logo" />
-      <img className="sub-logo" src={LogoSub} alt="wolfinger" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/grzegorz-wolfinger-b88856229/"
-        >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/wujekbizon"
-        >
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+const Sidebar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
+
+  return (
+    <div id="nav_bar" className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoG} alt="logo" />
+        <img className="sub-logo" src={LogoSub} alt="wolfinger" />
+      </Link>
+      <div id="navBar">
+        <nav className={`menuNav ${navbarOpen ? 'showMenu' : ''}`}>
+          <NavLink
+            onClick={() => closeMenu()}
+            exact="true"
+            activeclassname="active"
+            to="/"
+          >
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            onClick={() => closeMenu()}
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            onClick={() => closeMenu()}
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/grzegorz-wolfinger-b88856229/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/wujekbizon"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+        <button onClick={handleToggle}>
+          {navbarOpen ? (
+            <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+          ) : (
+            <FiMenu
+              style={{ color: '#7b7b7b', width: '40px', height: '40px' }}
+            />
+          )}
+        </button>
+      </div>
+    </div>
+  )
+}
 
 export default Sidebar
